5cc3cb7bcaffa68e6740a3dae315577762961130
Alejoab <aalvarezbo@unal.edu.co>
2023-06-01 11:38:55 -0500

    Added generarEstadisticas

--- src/Funciones/GenerarEstadisticas.java
+++ src/Funciones/GenerarEstadisticas.java
@@ -3,69 +3,78 @@ package Funciones;
 import Conexiones.MongoDB;
 import Conexiones.Oracle;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
 public class GenerarEstadisticas {
 
-    private String queryPais = 'SELECT CIUDAD.NOM_PAIS, SUM(VENTA.NUMERO_UNIDADES * VENTA.RECIO_UNITARIO) AS TOTAL_VENTAS FROM VENTA, JOIN SUCURSAL ON VENTA.COD_SUC = SUCURSAL.CODIGO_SUC, JOIN CIUDAD ON SUCURSAL.NOM_CIUDAD = CIUDAD.NOM_CIUDAD, TABLE(DETALLES) WHERE VENTA.ESTADO = "No Procesada" GROUP BY CIUDAD.NOM_PAIS';
-    private String queryEtario = 'SELECT CATEGORIA.NOMBRE_CATEGORIA, CLIENTE.GRUPO_ETARIO, SUM(VENTA.NUMERO_UNIDADES * VENTA.RECIO_UNITARIO) AS TOTAL_VENTAS FROM VENTA, JOIN CLIENTE ON VENTA.PASAPORTE = CLIENTE.PASAPORTE, JOIN PRODUCTO ON VENTA.CODIGO_PRO = PRODUCTO.CODIGO_PRO, JOIN CATEGORIA ON PRODUCTO.CODIGO_CAT = CATEGORIA.CODIGO_CAT, TABLE(DETALLERS) WHERE VENTA.ESTADO = "No Procesada" GROUP BY CATEGORIA.NOMBRE_CATEGORIA, CLIENTE.GRUPO_ETARIO';
+    private String queryPais = "SELECT CIUDAD.NOM_PAIS, SUM(VENTA.NUMERO_UNIDADES * VENTA.RECIO_UNITARIO) AS TOTAL_VENTAS FROM VENTA, JOIN SUCURSAL ON VENTA.COD_SUC = SUCURSAL.CODIGO_SUC, JOIN CIUDAD ON SUCURSAL.NOM_CIUDAD = CIUDAD.NOM_CIUDAD, TABLE(DETALLES) WHERE VENTA.ESTADO = \"No Procesada\" GROUP BY CIUDAD.NOM_PAIS";
+    private String queryEtario = "SELECT CATEGORIA.NOMBRE_CATEGORIA, CLIENTE.GRUPO_ETARIO, SUM(VENTA.NUMERO_UNIDADES * VENTA.RECIO_UNITARIO) AS TOTAL_VENTAS FROM VENTA, JOIN CLIENTE ON VENTA.PASAPORTE = CLIENTE.PASAPORTE, JOIN PRODUCTO ON VENTA.CODIGO_PRO = PRODUCTO.CODIGO_PRO, JOIN CATEGORIA ON PRODUCTO.CODIGO_CAT = CATEGORIA.CODIGO_CAT, TABLE(DETALLERS) WHERE VENTA.ESTADO = \"No Procesada\" GROUP BY CATEGORIA.NOMBRE_CATEGORIA, CLIENTE.GRUPO_ETARIO";
     private String updateProcesados = "UPDATE VENTA WHERE ESTADO = \"No Procesada\" SET ESTADO = \"Procesada\"";
 
-    public void generarPais() {
+    private String coleccionPais = "totalventasporpais";
+    private String coleccionEtario = "totalventasporcatygedad";
+
+
+    public void generarEstadisticas() {
         try {
-            Oracle oracle = new Oracle();
-            MongoDB mongo = new MongoDB();
-
-            Statement sentencia = oracle.conn.createStatement();
-            ResultSet resultado;
-
-            resultado = sentencia.executeQuery(this.queryPais);
-
-            if (!mongoDb.listCollectionNames().into(new ArrayList<>()).contains("totalventasporpais")) {
-                mongoDb.createCollection("totalventasporpais");
-            }
-
-            MongoCollection<Document> coleccion = mongoDb.getCollection("totalventasporpais");
-
-            //TODO: Agregar o editar documentos en la coleccion
-
-
-
-            oracle.conn.close();
+            generarPais();
+            generarEtario();
+            actualizarProcesados();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void generarEtario() {
-        try {
-            Oracle oracle = new Oracle();
-            MongoDB mongo = new MongoDB();
+    private void generarPais() throws Exception {
+        Oracle oracle = new Oracle();
+        MongoDB mongo = new MongoDB();
 
-            Statement sentencia = oracle.conn.createStatement();
-            ResultSet resultado;
+        Statement sentencia = oracle.conn.createStatement();
+        ResultSet resultado = sentencia.executeQuery(this.queryPais);
 
-            resultado = sentencia.executeQuery(this.queryEtario);
-
-            if (!mongoDb.listCollectionNames().into(new ArrayList<>()).contains("totalventasporpais")) {
-                mongoDb.createCollection("totalventasporpais");
-            }
-
-            MongoCollection<Document> coleccion = mongoDb.getCollection("totalventasporpais");
-
-            //TODO: Agregar o editar documentos en la coleccion
-
-
-
-            oracle.conn.close();
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        if (!mongoDb.listCollectionNames().into(new ArrayList<>()).contains(this.coleccionPais)) {
+            mongoDb.createCollection(this.coleccionPais);
         }
+
+        MongoCollection<Document> coleccion = mongoDb.getCollection(this.coleccionPais);
+
+        //TODO: Agregar o editar documentos en la coleccion
+
+
+        oracle.conn.close();
+    }
+
+    private void generarEtario() throws Exception {
+        Oracle oracle = new Oracle();
+        MongoDB mongo = new MongoDB();
+
+        Statement sentencia = oracle.conn.createStatement();
+        ResultSet resultado;
+
+        resultado = sentencia.executeQuery(this.queryEtario);
+
+        if (!mongoDb.listCollectionNames().into(new ArrayList<>()).contains(this.coleccionEtario)) {
+            mongoDb.createCollection(this.coleccionEtario);
+        }
+
+        MongoCollection<Document> coleccion = mongoDb.getCollection(this.coleccionEtario);
+
+        //TODO: Agregar o editar documentos en la coleccion
+
+
+        oracle.conn.close();
+    }
+
+    private void actualizarProcesados() throws Exception {
+        Oracle oracle = new Oracle();
+        MongoDB mongo = new MongoDB();
+
+        Statement sentencia = oracle.conn.createStatement();
+        sentencia.executeQuery(this.updateProcesados);
+
+        oracle.conn.close();
     }
 }
